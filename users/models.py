c234f809841b3dd37b8b37856402dfb7d1549d4d
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-05-24 20:58:13 -0600

    Adding models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+""" Users Models"""
+# Django
+
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Teacher(models.Model):
+    pass
+
+
+class Subject(models.Model):
+    pass
+
+
+class Student(models.Model):
+    pass
 
 # Create your models here.
