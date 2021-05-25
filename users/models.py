50f488ad61758de67e44ef2f548ffdd18f6410db
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-05-24 21:10:05 -0600

    Creating teacher, student and subject classes

--- users/models.py
+++ users/models.py
@@ -6,7 +6,16 @@ from django.contrib.auth.models import User
 
 
 class Teacher(models.Model):
-    pass
+    teacher = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = {'title', 'created_at'}
+
+    def __str__(self):
+        return self.teacher.get_full_name()
 
 
 class Subject(models.Model):
@@ -14,6 +23,22 @@ class Subject(models.Model):
 
 
 class Student(models.Model):
-    pass
+    student = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_number = models.CharField(max_length=100, blank=True)
+    subject = models.ManyToManyField(Subject)
+    group = models.CharField(max_length=100, blank=True)
 
+    CAREERS = [
+        ("TI", "TI"),
+        ("DN", "DN"),
+        ("PI", "PI")
+    ]
+
+    career = models.CharField(choices=CAREERS, max_lenght=50)
+
+    class Meta:
+        ordering = ['group', 'career', 'student_number']
+
+    def __str__(self):
+        return self.student.get_full_name()
 # Create your models here.
